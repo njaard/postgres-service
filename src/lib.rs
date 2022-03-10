986998eb39a5b7114cb11b40c89160cf31bebea9
Daniel Brooks <db48x@db48x.net>
2022-03-09 22:42:54 -0800

    Add support for the tokio-postgres crate
    The tokio-postgres crate has a different Config type, so add a second
    function for getting it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,77 @@
-use ini::Ini;
+//! Parse Postgres service configuration files
+
+#![feature(doc_cfg)]
+
 use ini::Properties;
-use postgres::config::Config;
 
-fn build_from_section(section: &Properties)
-	-> Config
-{
-	let mut username: Option<String> = None;
-	let mut password: Option<String> = None;
+fn build_from_section(section: &Properties) -> tokio_postgres::config::Config {
+    let mut username: Option<String> = None;
+    let mut password: Option<String> = None;
 
-	let mut builder = Config::new();
-	let mut options = String::new();
+    let mut builder = tokio_postgres::config::Config::new();
+    let mut options = String::new();
 
-	for (k,v) in section.iter()
-	{
-		match k
-		{
-			"host" =>
-				{ builder.host(v); },
-			"hostaddr" => 
-				{ builder.host(v); },
-			"port" =>
-				{ builder.port(v.parse().unwrap()); },
-			"dbname" =>
-				{ builder.dbname(v); },
-			"user" =>
-				username = Some(v.to_owned()),
-			"password" =>
-				password = Some(v.to_owned()),
-			_ =>
-				options += &format!("{}={} ", k, v),
-		}
-	}
+    for (k, v) in section.iter() {
+        match k {
+            "host" => {
+                builder.host(v);
+            }
+            "hostaddr" => {
+                builder.host(v);
+            }
+            "port" => {
+                builder.port(v.parse().unwrap());
+            }
+            "dbname" => {
+                builder.dbname(v);
+            }
+            "user" => username = Some(v.to_owned()),
+            "password" => password = Some(v.to_owned()),
+            _ => options += &format!("{}={} ", k, v),
+        }
+    }
 
-	if !options.is_empty()
-		{ builder.options(&options); }
+    if !options.is_empty() {
+        builder.options(&options);
+    }
 
-	if let Some(username) = username
-		{ builder.user(&username); }
-	if let Some(password) = password
-		{ builder.password(&password); }
+    if let Some(username) = username {
+        builder.user(&username);
+    }
+    if let Some(password) = password {
+        builder.password(&password);
+    }
 
-	builder
+    builder
 }
 
-pub fn load_connect_params(
-	service_name : &str
-) -> Option<Config>
-{
-	if let Ok(home) = std::env::var("HOME")
-	{
-		if let Ok(ini) = Ini::load_from_file(home + "/" + ".pg_service.conf")
-		{
-			if let Some(section) = ini.section(Some(service_name.clone()))
-			{
-				return Some(build_from_section(section));
-			}
-		}
-	}
-
-	let confdir = std::env::var("PGSYSCONFDIR").unwrap_or("/etc/postgresql-common".into());
-
-	if let Ok(ini) = Ini::load_from_file(confdir + "/" + "pg_service.conf")
-	{
-		if let Some(section) = ini.section(Some(service_name))
-		{
-			return Some(build_from_section(section));
-		}
-	}
-
-	None
+/// Load connection parameters for the named Postgresql service
+pub fn load_connect_params(service_name: &str) -> Option<postgres::config::Config> {
+    tokio::load_connect_params(service_name).map(Into::into)
 }
 
+/// For use with tokio-postgres
+pub mod tokio {
+    use crate::build_from_section;
+    use ini::Ini;
+    /// Load connection parameters for the named Postgresql service
+    pub fn load_connect_params(service_name: &str) -> Option<tokio_postgres::config::Config> {
+        if let Ok(home) = std::env::var("HOME") {
+            if let Ok(ini) = Ini::load_from_file(home + "/" + ".pg_service.conf") {
+                if let Some(section) = ini.section(Some(service_name.clone())) {
+                    return Some(build_from_section(section));
+                }
+            }
+        }
+
+        let confdir = std::env::var("PGSYSCONFDIR").unwrap_or("/etc/postgresql-common".into());
+
+        if let Ok(ini) = Ini::load_from_file(confdir + "/" + "pg_service.conf") {
+            if let Some(section) = ini.section(Some(service_name)) {
+                return Some(build_from_section(section));
+            }
+        }
+
+        None
+    }
+}
